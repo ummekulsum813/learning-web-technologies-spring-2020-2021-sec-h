dcc975e3527d6b03def084d598541fe9d283f6d8
Umme Kulsum <ummekulsum813@gmail.com>
2021-02-23 12:32:53 +0600

    task commite

---
+++ MID_LAB_TASK_PHP_04/task1.php
@@ -0,0 +1,12 @@
+<?php
+
+$length = 3;
+$width = 5;
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+
+echo "Area of rectangle: $area";
+echo "\r\n";
+echo "perimeter of a rectangle : $perimeter";
+
+?>
